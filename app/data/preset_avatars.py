0b0ef3b29fae45aaeb34e642c507aef9defef251
suma <suma@example.com>
2026-04-28 14:54:52 +0800

    替换为14个新数字人预设形象

--- app/data/preset_avatars.py
+++ app/data/preset_avatars.py
@@ -2,42 +2,114 @@
 PRESET_AVATARS = [
     {
         "id": 1,
-        "name": "专业新闻主播",
-        "category": "新闻主播",
-        "preview_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/news_host.jpg",
-        "model_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/news_host_model.jpg",
-        "description": "端庄大气的新闻主播形象"
+        "name": "文慧",
+        "category": "知识科普",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E6%96%87%E6%85%A7.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E6%96%87%E6%85%A7.png",
+        "description": "25岁知识女性，知性专业"
     },
     {
         "id": 2,
-        "name": "活力游戏主播",
-        "category": "游戏主播",
-        "preview_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/game_host.jpg",
-        "model_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/game_host_model.jpg",
-        "description": "充满活力的游戏主播形象"
+        "name": "佳慧",
+        "category": "生活分享",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E4%BD%B3%E6%85%A7.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E4%BD%B3%E6%85%A7.png",
+        "description": "22岁邻家女孩，温暖治愈"
     },
     {
         "id": 3,
-        "name": "AI带货主播",
-        "category": "带货主播",
-        "preview_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/shop_host.jpg",
-        "model_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/shop_host_model.jpg",
-        "description": "专业的带货主播形象"
+        "name": "玲美",
+        "category": "情感问答",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E7%8E%B2%E7%BE%8E.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E7%8E%B2%E7%BE%8E.png",
+        "description": "28岁知性姐姐，富有共情力"
     },
     {
         "id": 4,
-        "name": "可爱卡通",
-        "category": "卡通形象",
-        "preview_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/cartoon.jpg",
-        "model_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/cartoon_model.jpg",
-        "description": "可爱的卡通人物形象"
+        "name": "晴依",
+        "category": "活动主持",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E6%99%B4%E4%BE%9D.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E6%99%B4%E4%BE%9D.png",
+        "description": "26岁晚礼服形象，大方得体"
     },
     {
         "id": 5,
-        "name": "动物形象",
-        "category": "动物形象",
-        "preview_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/animal.jpg",
-        "model_image": "https://lingjing-media.oss-cn-shenzhen.aliyuncs.com/presets/animal_model.jpg",
-        "description": "可爱的动物形象"
+        "name": "燃锋",
+        "category": "电竞主播",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E7%87%83%E9%94%8B.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E7%87%83%E9%94%8B.png",
+        "description": "20岁潮流男生，有激情"
+    },
+    {
+        "id": 6,
+        "name": "明悦",
+        "category": "新闻主播",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E6%98%8E%E6%82%A6.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E6%98%8E%E6%82%A6.png",
+        "description": "30岁干练女性，专业信赖"
+    },
+    {
+        "id": 7,
+        "name": "宇航",
+        "category": "节目主持",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E5%AE%87%E8%88%AA.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E5%AE%87%E8%88%AA.png",
+        "description": "32岁成熟稳重男性"
+    },
+    {
+        "id": 8,
+        "name": "安然",
+        "category": "电台主播",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E5%AE%89%E7%84%B6.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E5%AE%89%E7%84%B6.png",
+        "description": "35岁大姐姐，声音有磁性"
+    },
+    {
+        "id": 9,
+        "name": "小智",
+        "category": "企业服务",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E5%B0%8F%E6%99%BA.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E5%B0%8F%E6%99%BA.png",
+        "description": "30岁专业干练男性"
+    },
+    {
+        "id": 10,
+        "name": "美娜",
+        "category": "老师/教育",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E7%BE%8E%E5%A8%9C.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E7%BE%8E%E5%A8%9C.png",
+        "description": "25岁温柔女教师"
+    },
+    {
+        "id": 11,
+        "name": "雪雪",
+        "category": "趣味角色",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E9%9B%AA%E9%9B%AA.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E9%9B%AA%E9%9B%AA.png",
+        "description": "18岁可爱萌妹"
+    },
+    {
+        "id": 12,
+        "name": "露西",
+        "category": "美妆带货",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E9%9C%B2%E8%A5%BF.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E9%9C%B2%E8%A5%BF.png",
+        "description": "22岁辣妹，美妆达人"
+    },
+    {
+        "id": 13,
+        "name": "凯西",
+        "category": "萌宠/动物",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E5%87%AF%E8%A5%BF.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E5%87%AF%E8%A5%BF.png",
+        "description": "可爱的虎斑猫拟人"
+    },
+    {
+        "id": 14,
+        "name": "杏子",
+        "category": "老师/教育",
+        "preview_image": "https://media.lingjing-media.com/presets/OSS/%E6%9D%8F%E5%AD%90.png",
+        "model_image": "https://media.lingjing-media.com/presets/OSS/%E6%9D%8F%E5%AD%90.png",
+        "description": "28岁文艺风女老师"
     }
 ]
